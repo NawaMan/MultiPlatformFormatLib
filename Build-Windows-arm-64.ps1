62df20ffff58b1be4803db07f78937fcb3027d13
NawaMan <nawa@nawaman.net>
2025-05-21 11:50:29 -0400

    Experiment 4

--- Build-Windows-arm-64.ps1
+++ Build-Windows-arm-64.ps1
@@ -91,8 +91,8 @@ $OptFlags     = "-O2 -flto -ffunction-sections -fdata-sections"
 $LinkFlags    = "-Wl,--gc-sections"
 $TargetTriple = "aarch64-pc-windows-msvc"
 
-$env:CC       = "clang --target=$TargetTriple"
-$env:CXX      = "clang++ --target=$TargetTriple"
+# $env:CC       = "clang --target=$TargetTriple"
+# $env:CXX      = "clang++ --target=$TargetTriple"
 $env:CFLAGS   = $OptFlags
 $env:CXXFLAGS = $OptFlags
 $env:LDFLAGS  = $LinkFlags
@@ -111,8 +111,8 @@ cmake ..                                         `
     -DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreaded" `
     -DCMAKE_SYSTEM_NAME="Windows"                `
     -DCMAKE_SYSTEM_PROCESSOR="ARM64"             `
-    -DCMAKE_C_COMPILER="$CCompiler"              `
-    -DCMAKE_CXX_COMPILER="$CXXCompiler"          `
+    -DCMAKE_C_COMPILER="clang"                   `
+    -DCMAKE_CXX_COMPILER="clang++"               `
     -DCMAKE_C_FLAGS="--target=$TargetTriple $OptFlags"   `
     -DCMAKE_CXX_FLAGS="--target=$TargetTriple $OptFlags" `
     -DCMAKE_EXE_LINKER_FLAGS="$LinkFlags"                `
